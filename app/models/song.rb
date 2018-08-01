2a047b498f1aee602c7a58cec5186208f73febcf
Carrie Savinell <carrielane89@gmail.com>
2018-08-01 23:35:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,5 +5,5 @@ class Song < ActiveRecord::Base
   #integer
   #optional if released is false
   #must be <= current year
-  validates :release_year, numericality: { less_than_or_equal_to:  }
+  validates :release_year, numericality: { less_than_or_equal_to: Date.current.year }
 end
