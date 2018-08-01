f4b2aefba0df6885f0a95b92f3faca8444a89af1
Carrie Savinell <carrielane89@gmail.com>
2018-08-01 23:25:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: { scope: [:artist_name, :release_year}, message: "cannot be repeated by same author in same year" ]
+  validates :title, uniqueness: { scope: [:artist_name, :release_year], message: "cannot be repeated by same author in same year" }
 
 end
