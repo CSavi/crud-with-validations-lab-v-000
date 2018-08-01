45e55610d13573b0d2a98bf43d804f77c646a860
Carrie Savinell <carrielane89@gmail.com>
2018-08-01 23:37:01 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,5 @@ class Song < ActiveRecord::Base
   #optional if released is false
   #must be <= current year
   validates :release_year, numericality: { less_than_or_equal_to: Date.current.year }, if: :released
+  validates :artist_name, 
 end
